1a4497c378c95279dd9b23496403fa4bd6c63239
abstract-animation-9914 <vm930@hotmail.com>
2019-02-07 15:02:44 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,10 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board(board)
-  
+  Would Print:
+    |   |   
+-----------
+    | X |   
+-----------
+    |   |   
 end 
